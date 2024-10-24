9c48704ef766f543103d55503458117a9cdd3e6e
Tiago Gasque de Oliveira <133129966+tgasque@users.noreply.github.com>
2024-10-24 15:08:05 -0300

    commit master

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth';
+
+function authMiddleware(request, response, next) {
+    const authToken = request.headers.authorization;
+
+if (!authToken) {
+    return response.status(401).json({error: 'Token not provider'})
+}
+
+const token = authToken.split(' ').at(1);
+
+try {
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if (err) {
+            throw new Error()
+        } 
+
+        request.userId = decoded.id;
+        request.userName = decoded.name;
+       })
+} catch (err) {
+    return response.status(401).json({errer: 'Token is invalid'})
+}
+
+return next()
+}
+export default authMiddleware
---
+++ src/migrations/20241014154400-add-path-column.js
@@ -0,0 +1,13 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  await queryInterface.addColumn('categories', 'path', { 
+    type: Sequelize.STRING });
+    
+  },
+
+  async down (queryInterface) {
+     await queryInterface.dropTable('categories', 'path');
+    
+  }
+};
